b1034fd7bfe87daddb72a4e7f8c9f86d6fe3828e
Ryan Westlake <ryancwestlake@gmail.com>
2016-11-30 13:57:29 -0700

    Clean first successful redux to remove render

--- src/components/test.js
+++ src/components/test.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import { Test } from '../actions/index'
 import { store } from '../index'
 
 const Counter = ({ handleSumbit, state }) => {
   return (
     <div>
+      <p>Hello! This is a test. If you can read this, then React and webpack are working.</p>
       <p>{state}</p>
       <button onClick={handleSumbit}>Counter</button>
     </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,17 +3,6 @@ import { connect } from 'react-redux'
 import { test } from '../actions/index'
 import Counter from '../components/test'
 
-class App extends Component {
-  render() {
-    return (
-      <section>
-        <div>Hello! This is a test. If you can read this, then React and webpack are working.</div>
-        <Counter />
-      </section>
-    )
-  }
-}
-
 const mapStateToProps = (state) => ({
   state,
 })
