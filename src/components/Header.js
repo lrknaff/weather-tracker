707978b7a62a1485108189e883d5f0b039682aeb
gness1804 <graham.nessler@gmail.com>
2016-12-01 15:46:15 -0700

    got temp conversion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import convertKelvinToFahrenheit from './helpers/temp-conversion'
 
 class Header extends Component {
 
-
   render() {
     const { location, temp, weatherType } = this.props.state.getCurrentWeather
+    // const tempF = convertKelvinToFahrenheit(temp)
     let data
     if (this.props.state.getCurrentWeather.temp) {
-      data = <p>The current weather in {location} is {weatherType} and {temp}</p>
+      const tempF = Math.floor(convertKelvinToFahrenheit(temp))
+      data = <p>The current weather in {location} is {weatherType} and {tempF}</p>
     } else {
       data = 'Loading...'
     }
