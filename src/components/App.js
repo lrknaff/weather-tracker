8c93cff706440e758f9d3756334890d337738129
Ryan Westlake <ryancwestlake@gmail.com>
2016-11-30 22:17:24 -0700

    Remove bindActionCreators from App (unused)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import axios from 'axios'
 import HeaderContainer from '../containers/HeaderContainer'
 import { fetchForecast, updateLocation } from '../actions/index'
