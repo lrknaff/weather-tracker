dcda56a866b534a5be7ff89cfbade66add4b2c55
gness1804 <graham.nessler@gmail.com>
2016-12-01 20:26:37 -0700

    WIP - trying to get revised first test of header to pass

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,30 @@ import Loader from './loader'
 class Header extends Component {
 
   render() {
+    let location
+    let temp
+    let weatherType
+
     if (this.props.test) {
-      return <div className="test-class">The page renders!</div>
+      location = 'Denver'
+      temp = '30'
+      weatherType = 'Freakin Cold!'
+      // return <div className="test-class">The page renders!</div>
+    } else {
+      const realWeather = this.props.state.getCurrentWeather
+      location = realWeather.location
+      temp = realWeather.temp
+      weatherType = realWeather.weatherType
     }
-    const { location, temp, weatherType } = this.props.state.getCurrentWeather
+    // const { location, temp, weatherType } = this.props.state.getCurrentWeather
     let data
     let loading
-    if (this.props.state.getCurrentWeather.temp) {
+    if (this.props.test || this.props.state.getCurrentWeather.temp) {
       const tempF = Math.floor(convertKelvinToFahrenheit(temp))
       data = (
         <div>
-          <h2>The current weather in {location} is {tempF} &deg;F and {weatherType}.</h2>
-          <Link to="/foo">View Extended Forecast</Link>
+          <h2 className="current-weather-headline">The current weather in {location} is {tempF} &deg;F and {weatherType}.</h2>
+          <Link className="extended-forecast-link" to="/foo">View Extended Forecast</Link>
         </div>)
     } else {
       data = ''
--- test/components/Header.test.js
+++ test/components/Header.test.js
@@ -3,10 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { shallow, mount, render } from 'enzyme'
 import Header from '../../src/components/Header'
+var locus = require('locus')
 
 describe('Header', () => {
   it('should render without crashing', () => {
     const wrapper = shallow(<Header test={true}/>)
-    expect(wrapper.find('.test-class').text()).toEqual('The page renders!')
+    eval(locus)
+    expect(wrapper.find('.extended-forecast-link').text()).toEqual('View Extended Forecast')
+    // expect(wrapper.contains('.extended-forecast-link')).toEqual(true)
   });
 });
