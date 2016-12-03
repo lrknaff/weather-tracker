e50320b8ff7e62e303ebadf7ab92f61f257152c9
lrknaff <lacey.knaff@gmail.com>
2016-12-03 11:49:44 -0700

    add weather type in lowercase to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 
 const Card = ({ state, id }) => {
+  console.log(state)
   return (
     <div>
-      <p>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].location : null}</p>
-      <p>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].temp : null}</p>
+      <h3>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].location : null}</h3>
+      <h2>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].temp : null}</h2>
+      <h4>{ state.getCurrentWeatherByZip[id] ? `with ${state.getCurrentWeatherByZip[id].weatherType.toLowerCase()}` : null}</h4>
       <p>{ state.getCurrentWeatherByZip[id] ? null : <Link to="/settings" >Add a pinned city.</Link> }</p>
     </div>
   )
