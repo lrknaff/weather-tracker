f916c6d656e1f068e409eef907ecbd98fa308914
Nick <nicknchambers@gmail.com>
2016-12-03 14:02:50 -0700

    add individual gradients to weaterh cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,28 @@ import { Link } from 'react-router'
 import convertKelvinToFahrenheit from './helpers/temp-conversion'
 
 const Card = ({ state, id }) => {
-  console.log(state)
-  let backgroundColor
-  if (id === 1) {
-    backgroundColor = 'purple'
+  console.log(id)
+  let newId
+  if (id === 0) {
+    newId = 'a'
+  } else if (id === 1) {
+    newId = 'b'
   } else if (id === 2) {
-    backgroundColor = 'blue'
-  } else if (id === 3) {
-    backgroundColor = 'magenta'
+    newId = 'c'
   }
+  // let backgroundColor
+  // let colorOne
+  // let colorTwo
+  // if (id === 0) {
+  //   backgroundColor = 'linear-gradient(to bottom, #7B5ECF, #869BFC);'
+  // } else if (id === 1) {
+  //   backgroundColor = 'blue'
+  // } else if (id === 2) {
+  //   backgroundColor = 'magenta'
+  // }
   return (
     <div className="card-container">
-      <div className="card-main-info" style={{ backgroundColor }}>
+      <div className="card-main-info" id={newId}>
         <h3>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].location : null}</h3>
         <h2>{ state.getCurrentWeatherByZip[id] ? Math.floor(convertKelvinToFahrenheit(state.getCurrentWeatherByZip[id].temp)) : null}</h2>
         <h4>{ state.getCurrentWeatherByZip[id] ? `and ${state.getCurrentWeatherByZip[id].weatherType.toLowerCase()}` : null}</h4>
