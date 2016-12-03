26c97cd0a6f0edc21fb25b120522a86a7ed2634b
Ryan Westlake <ryancwestlake@gmail.com>
2016-12-03 15:26:20 -0700

    Able to get day in the week from api data

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,7 +12,7 @@ const Card = ({ state, id }) => {
         <h3>{state.getCurrentWeatherByZip[id].location}</h3>
         <h2>{Math.floor(convertKelvinToFahrenheit(state.getCurrentWeatherByZip[id].temp))}</h2>
         <h4>{`and ${state.getCurrentWeatherByZip[id].weatherType.toLowerCase()}`}</h4>
-        <FiveDay />
+        <FiveDay state={state} />
       </div>
     )
   } else {
--- src/components/FiveDay.js
+++ src/components/FiveDay.js
@@ -2,17 +2,48 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import convertKelvinToFahrenheit from './helpers/temp-conversion'
 
-const FiveDay = ({ state, id }) => {
-  return (
-    <div>
-      <ul>
-        <li>day</li>
-      </ul>
-      <ul>
-        <li>temp</li>
-      </ul>
-    </div>
-  )
+const daysMap = {
+  '0': 'Sunday',
+  '1': 'Monday',
+  '2': 'Tuesday',
+  '3': 'Wednesday',
+  '4': 'Thursday',
+  '5': 'Friday',
+  '6': 'Saturday',
+}
+
+class FiveDay extends Component {
+
+  componentDidMount() {
+    this.getDays()
+  }
+
+  getDays() {
+    const { state } = this.props
+
+    const days = state.getFiveDayForecast[0].forecast.map((d) => {
+      return d.dt_txt.split(' ')[0].split('-')
+    })
+    console.log(days[0])
+
+    const date = new Date(parseInt(days[0][0], 10), parseInt(days[0][1] - 1, 10), parseInt(days[0][2], 10))
+    console.log(date)
+    console.log(date.getDay())
+  }
+
+  render() {
+    console.log(this.props.state)
+    return (
+      <div>
+        <ul>
+          <li>day</li>
+        </ul>
+        <ul>
+          <li>temp</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default FiveDay
