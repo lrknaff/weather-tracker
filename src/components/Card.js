5f03327268261876b91b6f435a7e5c76547aecca
Nick <nicknchambers@gmail.com>
2016-12-03 13:25:00 -0700

    add individual background-color styles for weather cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,24 @@ import convertKelvinToFahrenheit from './helpers/temp-conversion'
 
 const Card = ({ state, id }) => {
   console.log(state)
+  let backgroundColor
+  if (id === 1) {
+    backgroundColor = 'purple'
+  } else if (id === 2) {
+    backgroundColor = 'blue'
+  } else if (id === 3) {
+    backgroundColor = 'magenta'
+  }
   return (
     <div className="card-container">
-      <h3>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].location : null}</h3>
-      <h2>{ state.getCurrentWeatherByZip[id] ? Math.floor(convertKelvinToFahrenheit(state.getCurrentWeatherByZip[id].temp)) : null}</h2>
-      <h4>{ state.getCurrentWeatherByZip[id] ? `and ${state.getCurrentWeatherByZip[id].weatherType.toLowerCase()}` : null}</h4>
-      <p>{ state.getCurrentWeatherByZip[id] ? null : <Link to="/settings" >Add a pinned city.</Link> }</p>
+      <div className="card-main-info" style={{ backgroundColor }}>
+        <h3>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].location : null}</h3>
+        <h2>{ state.getCurrentWeatherByZip[id] ? Math.floor(convertKelvinToFahrenheit(state.getCurrentWeatherByZip[id].temp)) : null}</h2>
+        <h4>{ state.getCurrentWeatherByZip[id] ? `and ${state.getCurrentWeatherByZip[id].weatherType.toLowerCase()}` : null}</h4>
+      </div>
+      <div className="card-extended-info">
+        <p>{ state.getCurrentWeatherByZip[id] ? null : <Link to="/settings" >Add a pinned city.</Link> }</p>
+      </div>
     </div>
   )
 }
