3c6f493745fb90b4df53c6715d986ddbccc13815
Ryan Westlake <ryancwestlake@gmail.com>
2016-12-04 14:25:33 -0700

    Display temperatures in extended forecast

--- src/components/FiveDay.js
+++ src/components/FiveDay.js
@@ -2,29 +2,46 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import convertKelvinToFahrenheit from './helpers/temp-conversion'
 
-const daysMap = {
-  '0': 'Sunday',
-  '1': 'Monday',
-  '2': 'Tuesday',
-  '3': 'Wednesday',
-  '4': 'Thursday',
-  '5': 'Friday',
-  '6': 'Saturday',
-}
-
 class FiveDay extends Component {
+
+  getMinAndMax(arr, day) {
+    const temps = day.map((hour) => {
+      return arr.push(Math.floor(convertKelvinToFahrenheit(hour.temp)))
+    })
+
+    console.log(arr)
+  }
+
   render() {
     let data
     const { state, id } = this.props
 
     if (state.getFiveDayForecast[id]) {
+      const dayOne = state.getFiveDayForecast[id].forecast.dayOne
+      const dayTwo = state.getFiveDayForecast[id].forecast.dayTwo
+      const dayThree = state.getFiveDayForecast[id].forecast.dayThree
+      const dayFour = state.getFiveDayForecast[id].forecast.dayFour
+
+      const one = []
+      const two = []
+      const three = []
+      const four = []
+
+      this.getMinAndMax(one, dayOne)
+      this.getMinAndMax(two, dayTwo)
+      this.getMinAndMax(three, dayThree)
+      this.getMinAndMax(four, dayFour)
+
       data = (
         <div>
           <ul>
-            <li>{state.getFiveDayForecast[id].forecast.dayOne[id].main}</li>
+            <li>{state.getFiveDayForecast[id].forecast.dayOne[id].day}</li>
+            <li>{state.getFiveDayForecast[id].forecast.dayTwo[id].day}</li>
+            <li>{state.getFiveDayForecast[id].forecast.dayThree[id].day}</li>
+            <li>{state.getFiveDayForecast[id].forecast.dayFour[id].day}</li>
           </ul>
           <ul>
-            <li>temp</li>
+            <li>{Math.max(...one)}&deg; / {Math.min(...one)}&deg;</li>
           </ul>
         </div>
       )
