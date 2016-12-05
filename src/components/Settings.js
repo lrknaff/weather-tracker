82973e830c45a105b711c269264877d35a667553
gness1804 <graham.nessler@gmail.com>
2016-12-04 22:58:34 -0700

    Made Settings a smart component so it can have state

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,50 +1,100 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
-const Settings = ({ fetchForecastByZip, state, removePinnedCity }) => {
-  let input
-  const cities = state.getCurrentWeatherByZip ?
-                 state.getCurrentWeatherByZip.map((city, i) => {
-                   return (
-                     <div key={i}>
-                       <button
-                         className="remove-button"
-                         onClick={() => removePinnedCity(i)} // pass in id
-                       >
-                         X
-                       </button>
-                       <p className="location">{city.location}</p>
-                     </div>
-                   )
-                 }) :
-                 null
+class Settings extends Component {
 
-  return (
-    <div className="settings-container">
-      <h1 className="settings-headline">My Cities</h1>
-      {cities}
-      <input
-        type="number"
-        placeholder="Enter a zip code"
-        value={input}
-        ref={(node) => { input = node }}
-      />
-      <button
-        onClick={(e) => {
-          e.preventDefault()
-          fetchForecastByZip(input.value)
-          input.value = ''
-        }}
-        className="add-city-button"
-      >Submit</button>
-      <p className="back-to-home-link-from-settings"><Link to="/">Back to Home &raquo;</Link></p>
-    </div>
-  )
-}
+  render() {
+    const { state, removePinnedCity, fetchForecastByZip } = this.props
 
-Settings.propTypes = {
-  fetchForecastByZip: PropTypes.func.isRequired,
-  removePinnedCity: PropTypes.func.isRequired,
-}
+    let input
+    const cities = state.getCurrentWeatherByZip ?
+                   state.getCurrentWeatherByZip.map((city, i) => {
+                     return (
+                       <div key={i}>
+                         <button
+                           className="remove-button"
+                           onClick={() => removePinnedCity(i)} // pass in id
+                         >
+                           X
+                         </button>
+                         <p className="location">{city.location}</p>
+                       </div>
+                     )
+                   }) :
+                   null
+
+    return (
+      <div className="settings-container">
+        <h1 className="settings-headline">My Cities</h1>
+        {cities}
+        <input
+          type="number"
+          placeholder="Enter a zip code"
+          value={input}
+          ref={(node) => { input = node }}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+            fetchForecastByZip(input.value)
+            input.value = ''
+          }}
+          className="add-city-button"
+        >Submit</button>
+        <p className="back-to-home-link-from-settings"><Link to="/">Back to Home &raquo;</Link></p>
+      </div>
+    )
+  }
+  }
+
 
 export default Settings
+
+
+// const Settings = ({ fetchForecastByZip, state, removePinnedCity }) => {
+//   let input
+//   const cities = state.getCurrentWeatherByZip ?
+//                  state.getCurrentWeatherByZip.map((city, i) => {
+//                    return (
+//                      <div key={i}>
+//                        <button
+//                          className="remove-button"
+//                          onClick={() => removePinnedCity(i)} // pass in id
+//                        >
+//                          X
+//                        </button>
+//                        <p className="location">{city.location}</p>
+//                      </div>
+//                    )
+//                  }) :
+//                  null
+//
+// //   return (
+// //     <div className="settings-container">
+// //       <h1 className="settings-headline">My Cities</h1>
+// //       {cities}
+// //       <input
+// //         type="number"
+// //         placeholder="Enter a zip code"
+// //         value={input}
+// //         ref={(node) => { input = node }}
+// //       />
+// //       <button
+// //         onClick={(e) => {
+// //           e.preventDefault()
+// //           fetchForecastByZip(input.value)
+// //           input.value = ''
+// //         }}
+// //         className="add-city-button"
+// //       >Submit</button>
+// //       <p className="back-to-home-link-from-settings"><Link to="/">Back to Home &raquo;</Link></p>
+// //     </div>
+// //   )
+// // }
+//
+// Settings.propTypes = {
+//   fetchForecastByZip: PropTypes.func.isRequired,
+//   removePinnedCity: PropTypes.func.isRequired,
+// }
+//
+// export default Settings
