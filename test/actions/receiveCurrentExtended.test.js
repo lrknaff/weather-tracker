0beb0cff6c2d7c8dffbde09388817f0730848764
Ryan Westlake <ryancwestlake@gmail.com>
2016-12-05 14:32:40 -0700

    All tests pass

--- test/actions/receiveCurrentExtended.test.js
+++ test/actions/receiveCurrentExtended.test.js
@@ -1,40 +1,27 @@
 import * as actions from '../../src/actions'
+import data from '../helpers/fakeJSON'
 
 describe('receive forecast action', () => {
   it('should return a forecast array with a, b, c, d', () => {
-    const json = {
-      data: {
-        list: [
-          {
-            dt: 1902020222,
-            dt_txt: '2016-12-04',
-          },
-          {
-            dt: 190303033,
-            dt_txt: '2016-12-04',
-          },
-          {
-            dt: 333233333,
-            dt_txt: '2016-12-04',
-          },
-          {
-            dt: 190203030,
-            dt_txt: '2016-12-04',
-          },
-        ],
-      },
-    }
-
     const expected = {
       type: 'RECEIVE_CURRENT_EXTENDED',
       data: {
-        a: [],
+        a: [
+          {
+            time: '0 AM',
+            date: '2016-12-06',
+            day: 'Tuesday',
+            temp: 275.97,
+            main: 'Clear',
+            description: 'clear sky',
+          },
+        ],
         b: [],
         c: [],
         d: [],
       },
     }
 
-    expect(actions.receiveCurrentExtendedForecast(json)).toEqual(expected)
+    expect(actions.receiveCurrentExtendedForecast(data)).toEqual(expected)
   })
 })
