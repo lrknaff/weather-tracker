7a18a9ccf172234901223289624f7f6f9772fbba
gness1804 <graham.nessler@gmail.com>
2016-12-04 20:41:47 -0700

    WIP - working on extended

--- src/components/Extended.js
+++ src/components/Extended.js
@@ -18,13 +18,14 @@ const ThreeHour = ({ state, params }) => {
   const displayData = dataArray.map((day) => {
     return day.map((hour, i) => {
       return (
-        <div key={i} className="extended-forecast-each-card" id={hour.day}>
+        <div key={i} className="extended-forecast-each-card" id={i}>
           <ul>
             <li>{hour.day}</li>
             <li>{hour.time}</li>
             <li>{Math.floor(convertKelvinToFahrenheit(hour.temp))}&deg;</li>
             <li>{hour.description}</li>
           </ul>
+          <br /><br /><br />
         </div>
       )
     })
