d276a8b14d169663db9ca222602d4c60416173fa
gness1804 <graham.nessler@gmail.com>
2016-12-04 23:00:39 -0700

    submit is isabled

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,9 +2,16 @@ import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
 class Settings extends Component {
+  constructor() {
+    super()
+    this.state = {
+      submitDisabled: true,
+    }
+  }
 
   render() {
     const { state, removePinnedCity, fetchForecastByZip } = this.props
+    const { submitDisabled } = this.state
 
     let input
     const cities = state.getCurrentWeatherByZip ?
@@ -40,6 +47,7 @@ class Settings extends Component {
             input.value = ''
           }}
           className="add-city-button"
+          disabled={submitDisabled}
         >Submit</button>
         <p className="back-to-home-link-from-settings"><Link to="/">Back to Home &raquo;</Link></p>
       </div>
@@ -49,52 +57,3 @@ class Settings extends Component {
 
 
 export default Settings
-
-
-// const Settings = ({ fetchForecastByZip, state, removePinnedCity }) => {
-//   let input
-//   const cities = state.getCurrentWeatherByZip ?
-//                  state.getCurrentWeatherByZip.map((city, i) => {
-//                    return (
-//                      <div key={i}>
-//                        <button
-//                          className="remove-button"
-//                          onClick={() => removePinnedCity(i)} // pass in id
-//                        >
-//                          X
-//                        </button>
-//                        <p className="location">{city.location}</p>
-//                      </div>
-//                    )
-//                  }) :
-//                  null
-//
-// //   return (
-// //     <div className="settings-container">
-// //       <h1 className="settings-headline">My Cities</h1>
-// //       {cities}
-// //       <input
-// //         type="number"
-// //         placeholder="Enter a zip code"
-// //         value={input}
-// //         ref={(node) => { input = node }}
-// //       />
-// //       <button
-// //         onClick={(e) => {
-// //           e.preventDefault()
-// //           fetchForecastByZip(input.value)
-// //           input.value = ''
-// //         }}
-// //         className="add-city-button"
-// //       >Submit</button>
-// //       <p className="back-to-home-link-from-settings"><Link to="/">Back to Home &raquo;</Link></p>
-// //     </div>
-// //   )
-// // }
-//
-// Settings.propTypes = {
-//   fetchForecastByZip: PropTypes.func.isRequired,
-//   removePinnedCity: PropTypes.func.isRequired,
-// }
-//
-// export default Settings
