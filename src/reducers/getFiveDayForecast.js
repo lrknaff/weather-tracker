a5334f50f91c46a92cd3786e83d6163c4ce93226
Ryan Westlake <ryancwestlake@gmail.com>
2016-12-04 17:32:46 -0700

    Write removePinned action

--- src/reducers/getFiveDayForecast.js
+++ src/reducers/getFiveDayForecast.js
@@ -2,6 +2,10 @@ const getFiveDayForecast = (state = [], action) => {
   switch (action.type) {
     case 'RECEIVE_FIVEDAY_FORECAST':
       return [...state, { forecast: action.data }]
+    case 'REMOVE_PINNED_CITY':
+      return [
+        ...state,
+      ]
     default:
       return state
   }
