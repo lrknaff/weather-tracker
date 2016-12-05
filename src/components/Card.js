fc1cf8cae8c6d5cde6359067e8312404a811c235
lrknaff <lacey.knaff@gmail.com>
2016-12-04 20:12:39 -0700

    add style for main card extended

--- src/components/Card.js
+++ src/components/Card.js
@@ -19,8 +19,10 @@ const Card = ({ state, id, ifHidden }) => {
     <div className={ifHidden}>
       <div className="card-main-info" id={newId}>
         <h3>{ state.getCurrentWeatherByZip[id] ? state.getCurrentWeatherByZip[id].location : null}</h3>
-        <h2>{ state.getCurrentWeatherByZip[id] ? Math.floor(convertKelvinToFahrenheit(state.getCurrentWeatherByZip[id].temp)) : null}</h2>
-        <h4>{ state.getCurrentWeatherByZip[id] ? `and ${state.getCurrentWeatherByZip[id].weatherType.toLowerCase()}` : null}</h4>
+        <h2 className="current-weather-temp">{ state.getCurrentWeatherByZip[id] ?
+          Math.floor(convertKelvinToFahrenheit(state.getCurrentWeatherByZip[id].temp)) : null}</h2>
+        <h4>{ state.getCurrentWeatherByZip[id] ?
+          `and ${state.getCurrentWeatherByZip[id].weatherType.toLowerCase()}` : null}</h4>
       </div>
       <div className="card-extended-info">
         { state.getCurrentWeatherByZip[id] ? null :
--- src/components/FiveDay.js
+++ src/components/FiveDay.js
@@ -32,17 +32,17 @@ class FiveDay extends Component {
 
       data = (
         <div className="five-day">
-          <ul>
-            <li>{state.getFiveDayForecast[id].forecast.a[id].day}</li>
-            <li>{state.getFiveDayForecast[id].forecast.b[id].day}</li>
-            <li>{state.getFiveDayForecast[id].forecast.c[id].day}</li>
-            <li>{state.getFiveDayForecast[id].forecast.d[id].day}</li>
+          <ul className="five-day-day-ul">
+            <li className="five-day-day">{state.getFiveDayForecast[id].forecast.a[id].day}</li>
+            <li className="five-day-day">{state.getFiveDayForecast[id].forecast.b[id].day}</li>
+            <li className="five-day-day">{state.getFiveDayForecast[id].forecast.c[id].day}</li>
+            <li className="five-day-day">{state.getFiveDayForecast[id].forecast.d[id].day}</li>
           </ul>
-          <ul>
-            <li>{Math.max(...one)}&deg; / {Math.min(...one)}&deg;</li>
-            <li>{Math.max(...two)}&deg; / {Math.min(...two)}&deg;</li>
-            <li>{Math.max(...three)}&deg; / {Math.min(...three)}&deg;</li>
-            <li>{Math.max(...four)}&deg; / {Math.min(...four)}&deg;</li>
+          <ul className="five-day-temp-ul">
+            <li className="five-day-temp">{Math.max(...one)}&deg; / {Math.min(...one)}&deg;</li>
+            <li className="five-day-temp">{Math.max(...two)}&deg; / {Math.min(...two)}&deg;</li>
+            <li className="five-day-temp">{Math.max(...three)}&deg; / {Math.min(...three)}&deg;</li>
+            <li className="five-day-temp">{Math.max(...four)}&deg; / {Math.min(...four)}&deg;</li>
           </ul>
           <button><Link to={`/Extended/${state.getCurrentWeatherByZip[id].location}/${id}`}>View Extended Forecast</Link></button>
         </div>
